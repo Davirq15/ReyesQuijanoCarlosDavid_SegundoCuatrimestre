83a91cfa275c1b64fea739b3ebec0719fb76127e
masegovi96 <mario.segovia.gtz@outlook.com>
2026-02-05 07:46:29 -0600

    Retroalimentación Tarea 2

--- Tarea 2/ejercicio4.php
+++ Tarea 2/ejercicio4.php
@@ -6,8 +6,10 @@ echo("<br><br><br>");
 
 $cadena = "super sigma";
 
+
 $cadena = str_replace(" ", "", strtolower($cadena));
 $conteo = array_count_values(str_split($cadena));// esto cuenta cuántas veces aparece cada letra
+// array = ["s", "u", "p", "e", "r", "s", "i", "g", "m", "a"]
 
 foreach ($conteo as $letra => $cantidad) {
     echo $letra . ": " . $cantidad . "<br>";
